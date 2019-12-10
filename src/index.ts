f322a93686af42ee935734e2661893e22a510592
Anthony-Umfer <Anthony.Umfer@coxautoinc.com>
2019-12-09 19:49:23 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,9 @@ const transactions = new Observable(observer => {
 transactions.subscribe();
 
 const httpServer = http.createServer(function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('hello, world!');
+    response.end();
 });
 httpServer.listen(80);
 
