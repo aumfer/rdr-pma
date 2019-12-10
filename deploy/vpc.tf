bc9eeb33aa8d283ff3f065e0afe91c3d7e72e8a7
Anthony-Umfer <Anthony.Umfer@coxautoinc.com>
2019-12-09 23:08:16 -0500

    Update vpc.tf

--- deploy/vpc.tf
+++ deploy/vpc.tf
@@ -7,7 +7,7 @@ data "aws_subnet_ids" "subnets" {
 }
 
 resource "aws_security_group" "security_group" {
-  vpc_id = "${data.aws_vpc.vpc.id}"
-  name   = "${module.tags.id}"
-  tags   = "${module.tags.tags}"
+  vpc_id = data.aws_vpc.vpc.id
+  name   = module.tags.id
+  tags   = module.tags.tags
 }
